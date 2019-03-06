ef4f3c8bdc1b044e19d466efb9ba6fc0a3af4220
Luca Boccassi <luca.boccassi@microsoft.com>
2019-03-06 11:43:38 +0000

    test

--- .ci/linux-build.sh
+++ .ci/linux-build.sh
@@ -11,6 +11,13 @@ function on_error() {
 }
 trap on_error ERR
 
+NINJA_PREFIX=""
+if [ -n "$SONAR" ]; then
+    NINJA_PREFIX="build-wrapper-linux-x86-64 --out-dir $PWD/bw-output "
+    # Sonarcloud can use git blame to pin-point when an issue was introduced
+    git fetch --unshallow
+fi
+
 if [ "${AARCH64}" == "1" ]; then
     # convert the arch specifier
     OPTS="${OPTS} -DRTE_ARCH_64=1 --cross-file config/arm/arm64_armv8_linuxapp_gcc"
@@ -18,4 +25,18 @@ fi
 
 OPTS="$OPTS --default-library=$DEF_LIB"
 meson build --werror -Dexamples=all ${OPTS}
-ninja -C build
+${NINJA_PREFIX} ninja -C build -j4
+
+if [ -n "$SONAR" ]; then
+    sonar-scanner -Dsonar.projectKey=bluca_dpdk \
+        -Dsonar.projectName=DPDK \
+        -Dsonar.projectVersion=1.0-SNAPSHOT \
+        -Dsonar.links.homepage=https://github.com/bluca/dpdk \
+        -Dsonar.links.ci=https://travis-ci.org/bluca/dpdk \
+        -Dsonar.links.scm=https://github.com/bluca/dpdk \
+        -Dsonar.links.issue=https://github.com/bluca/dpdk/issues \
+        -Dsonar.sources=. \
+        -Dsonar.cfamily.threads=4 \
+        -Dsonar.cfamily.build-wrapper-output=$PWD/bw-output \
+        -Dsonar.cfamily.gcov.reportsPath=.
+fi
